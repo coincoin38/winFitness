4feaf754a3dfe8139acc302ea8cc58a157f27e68
Julien Gimenez <jgimenez@sqli.com>
2016-10-10 16:48:20 +0200

    Detail news

--- myGymSwift/NewsDetailsViewController.swift
+++ myGymSwift/NewsDetailsViewController.swift
@@ -7,27 +7,34 @@
 //
 
 import UIKit
+import APESuperHUD
 
-class NewsDetailsViewController: UIViewController {
+class NewsDetailsViewController: UIViewController, UIWebViewDelegate {
     
     var news: FBFeedModel!
     var loaded : Bool = true
     let navBar = NavBarManager()
-    @IBOutlet weak var bodytNewsTextView: UITextView!
-    
+    @IBOutlet weak var bodyNewsWebView: UIWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setIHM()
     }
     
     func setIHM(){
-        bodytNewsTextView.isScrollEnabled = false
         
         title = "News du "+FormaterManager.SharedInstance.formatMMddFromDate(FormaterManager.SharedInstance.formatServerDateFromString(news.created_time!))
-        bodytNewsTextView.text = news.feedBody()
+        
         navBar.configureNavBarWithColors(navigationController!,
                                          backgroundColor: FormaterManager.SharedInstance.uicolorFromHexa(ColorsConstants.selectionTabBarColor),
                                          textColor: FormaterManager.SharedInstance.uicolorFromHexa(ColorsConstants.navBarTextAlternColor))
+        
+        
+        let bodyHtml = "<html><body style=\"margin: 0; padding: 0;\"><img src=\"%@\" style=\"width:100%%;\"><br><p style=\"text-align:justify;margin: 5; padding: 5;\">%@</p></body></html>";
+        let modifiedURLString = String(format: bodyHtml,news.full_picture!,news.feedBody())
+        bodyNewsWebView.delegate = self
+        bodyNewsWebView.loadHTMLString(modifiedURLString, baseURL: nil)
+        
     }
     
     override func viewWillAppear(_ animated: Bool)
@@ -39,15 +46,17 @@ class NewsDetailsViewController: UIViewController {
         }
     }
     
-    override func viewDidAppear(_ animated: Bool)
-    {
-        //Fix pour empêcher le scroll to bottom par défaut
-        bodytNewsTextView.isScrollEnabled = true
-    }
-    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(navigationController?.isNavigationBarHidden == false, animated: true)
         UIApplication.shared.statusBarStyle = .default
+    }
+    
+    func webViewDidStartLoad(_ webView : UIWebView) {
+        APESuperHUD.showOrUpdateHUD(loadingIndicator: .standard, message: NSLocalizedString("LOADING_NEW", comment:""), presentingView: self.view)
+    }
+    
+    func webViewDidFinishLoad(_ webView : UIWebView) {
+        APESuperHUD.removeHUD(animated: true, presentingView: self.view, completion:nil)
     }
 }
